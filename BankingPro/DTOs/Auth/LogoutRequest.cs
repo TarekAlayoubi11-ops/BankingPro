32e79549ed52da1e875d26a6fde125046cbad17e
Tarek Alayoubi <tarekalayoubi11@gmail.com>
2026-03-07 23:51:25 +0300

    Remove all files except README

--- BankingPro/DTOs/Auth/LogoutRequest.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BankingPro.DTOs.Auth
-{
-    public class LogoutRequest
-    {
-        public string? Username { get; set; }
-        public string? RefreshToken { get; set; }
-    }
-}
--- BankingPro/DTOs/Auth/TokenResponse.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BankingPro.DTOs.Auth
-{
-    public class TokenResponse
-    {
-        public string? AccessToken { get; set; }
-        public string? RefreshToken { get; set; }
-    }
-}
